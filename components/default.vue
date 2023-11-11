a64397a0ad41271b6d40abe7027022d952212677
Robin Lange <rlange@atlassian.com>
2023-11-11 22:10:02 +1300

    form upgrades

--- components/default.vue
+++ components/default.vue
@@ -36,38 +36,47 @@ let listItems: Array<ListItem> = [
     },
 ]
 
-let formData = {
+let formData = reactive({
     listName: '',
     listItemCount: 0,
     listItems: listItems,
-}
+});
 
 function objectKeysToArray(obj: object) {
     return Object.keys(obj);
 }
 
-
+function addListItem() {
+    formData.listItems.push({
+        id: uuidv4(),
+        title: '',
+        quantity: 1,
+        link: ''
+    });
+    console.log('listAdd', formData.listItems);
+}
 
 
 async function onSubmit(event: Event) {
     event?.preventDefault();
-    console.log(formData);
     await addDoc(collection(db, 'test'), formData);
 }
 
 </script>
 
 <template>
-    <div class="flex">
+    <div class="flex align-left justify-start">
+        <button @click="addListItem">Add Item</button>
         <form @submit="onSubmit">
-            <input type="text" name="name" v-model="formData.listName" />
-            <div class="inline-flex bg-red-600" v-for="item in formData.listItems" :key="item.id">
+
+            <div class="inline-flex mx-2 my-0 bg-red-600" v-for="item in formData.listItems" :key="item.id">
                 <span v-for="(field, index) in objectKeysToArray(item)" :key="field">
                     <div v-if="index > 0">
                         <input type="text" :name="field" :placeholder="field" />
                     </div>
                 </span>
             </div>
+
             <button type="submit" name="submit">Submit</button>
         </form>
     </div>
